13ad59fa63ee17fe35b1e18d887efc2c592dbfaa
unknown <pratikhamal.com@gmail.com>
2025-08-19 07:23:49 +0545

    Latest

---
+++ games.lua
@@ -0,0 +1,110 @@
+return {
+    ["110931811137535"] = {
+        n = "Feed & catch Brainrot",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/Feed%20%26%20catch%20Brainrot.lua",
+        u = 7824062257,
+        ["1"] = 7913,
+        ["2"] = 4732,
+        ["3"] = "getfenv",
+        ["4"] = 7509
+    },
+    ["115822424270425"] = {
+        n = "Raise a Farm",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/Raise%20a%20Farm.lua",
+        u = 7534084011,
+        ["1"] = 7214,
+        ["2"] = 3539,
+        ["3"] = "__index.same",
+        ["4"] = 8321
+    },
+    ["79546208627805"] = {
+        n = "99 Nights in the Forest",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/99Nights%20in%20the%20Forst.lua",
+        u = 7326934954,
+        ["1"] = 4122,
+        ["2"] = 3735,
+        ["3"] = "meta",
+        ["4"] = 6428
+    },
+    ["18172550962"] = {
+        n = "Pixel Blade [Early Access]",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/Pixel%20Blade.lua",
+        u = 6161049307,
+        ["1"] = 7819,
+        ["2"] = 4213,
+        ["3"] = "Get().efnc",
+        ["4"] = 9174
+    },
+    ["126884695634066"] = {
+        n = "[⛲] Grow a Garden 🍅",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/GrowAGarden.lua",
+        u = 7436755782,
+        ["1"] = 7352,
+        ["2"] = 2876,
+        ["3"] = "hook_e",
+        ["4"] = 5762
+    },
+    ["17589670912"] = {
+        n = "Find The Auras [585]",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/FindTheAura.lua",
+        u = 6022141304,
+        ["1"] = 8801,
+        ["2"] = 1244,
+        ["3"] = "G.getgc",
+        ["4"] = 6935
+    },
+    ["111989938562194"] = {
+        n = "Brainrot Evolution",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/BrainrotEvolution.lua",
+        u = 7332711118,
+        ["1"] = 7160,
+        ["2"] = 3333,
+        ["3"] = "function",
+        ["4"] = 8217
+    },
+    ["81440632616906"] = {
+        n = "⛏️ Dig to Earth's CORE!",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/DigToEarthCore.lua",
+        u = 7468338447,
+        ["1"] = 7382,
+        ["2"] = 3547,
+        ["3"] = "zzz",
+        ["4"] = 5093
+    },
+    ["94845773826960"] = {
+        n = "NEW🚪Dungeon Heroes ⚔️",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/DungeonHeroes.lua",
+        u = 7546582051,
+        ["1"] = 8194,
+        ["2"] = 4692,
+        ["3"] = "typeof(A.B.C())",
+        ["4"] = 7632
+    },
+    ["14313259147"] = {
+        n = "FORTLINE",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/FRONTLINE.lua",
+        u = 99999999,
+        ["1"] = 6998,
+        ["2"] = 3021,
+        ["3"] = "log",
+        ["4"] = 5841
+    },
+    ["15154513235"] = {
+        n = "💪Kamehameha Simulator",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/Kamehameha%20Simulator.lua",
+        u = 0,
+        ["1"] = 5993,
+        ["2"] = 1159,
+        ["3"] = "Crash",
+        ["4"] = 9467
+    },
+    ["132821440336521"] = {
+        n = "Merge For Diamods",
+        url = "https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/Merge%20for%20diamonds.lua",
+        u = 999,
+        ["1"] = 9922,
+        ["2"] = 9744,
+        ["3"] = "bloat",
+        ["4"] = 8274
+    }
+}
