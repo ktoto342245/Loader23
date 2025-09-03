454e23c4e434ae6ced6f94bd2e2c50e0160d3d4c
ktoto342245 <ktoto342245@gmail.com>
2025-09-03 15:30:16 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ktoto342245/Loader23/refs/heads/main/loader2.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ktoto342245/Scripts23/refs/heads/main/99Nights%20in%20the%20Forst.lua", true))()
