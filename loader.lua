820a348b141adc4a5c5fc30e9e285b7b10b6d901
ktoto342245 <ktoto342245@gmail.com>
2025-09-03 15:27:06 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/H4xScripts/Loader23/refs/heads/main/loader2.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ktoto342245/Loader23/refs/heads/main/loader2.lua", true))()
